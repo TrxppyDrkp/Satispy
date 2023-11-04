1b875388105ea7099133143c7e38f3a1633b95ed
TrxppyDrkp <144845525+TrxppyDrkp@users.noreply.github.com>
2023-11-04 08:52:47 -0500

    Add Satisfying.py

---
+++ Satisfying.py
@@ -0,0 +1,77 @@
+import time as t
+
+print("  ______    ______  ________  ______   ______   ________  __      __  ")
+print(" /      \  /      \|        \|      \ /      \ |        \|  \    /  \ ")
+print("|  $$$$$$\|  $$$$$$\\$$$$$$$$ \$$$$$$|  $$$$$$\| $$$$$$$$ \$$\  /  $$ ")
+print(" \$$    \ | $$    $$  | $$     | $$   \$$    \ | $$  \      \$$  $$   ")
+print(" \$$    \ | $$    $$  | $$     | $$   \$$    \ | $$  \      \$$  $$   ")
+print(" _\$$$$$$\| $$$$$$$$  | $$     | $$   _\$$$$$$\| $$$$$       \$$$$    ")
+print("|  \__| $$| $$  | $$  | $$    _| $$_ |  \__| $$| $$          | $$     ")
+print(" \$$    $$| $$  | $$  | $$   |   $$ \ \$$    $$| $$          | $$     ")
+print("  \$$$$$$  \$$   \$$   \$$    \$$$$$$  \$$$$$$  \$$           \$$     ")
+t.sleep(3)
+print("|----------------------------------------------------------------------|")
+print("|-----------------Made And Published By Trip/TrxppyDrkp----------------|")
+print("|----------------------------------------------------------------------|")
+t.sleep(3)
+
+
+# I'm New To Python, Please Don't Judge :] (ty)
+def Satisfying1():
+
+    while True:
+        print('|| T\\') #1 | Meeting Point 1.
+        t.sleep(0.1)
+        print('||\ H\\') #2.
+        t.sleep(0.1)
+        print('|| \ A\\') #3.
+        t.sleep(0.1)
+        print('||  \ N\\') #4.
+        t.sleep(0.1)
+        print('||   \ K\\') #5.
+        t.sleep(0.1)
+        print('||    \ Y\\') #6.
+        t.sleep(0.1)
+        print('||     \ O\\') #7.
+        t.sleep(0.1)
+        print('||      \ U\\') #8.
+        t.sleep(0.1)
+        print('||       \ F\\') #9.
+        t.sleep(0.1)
+        print('||        \ O\\') #10.
+        t.sleep(0.1)
+        print('||         \ U\\') #11.
+        t.sleep(0.1)
+        print('||          \ I\\') #12.
+        t.sleep(0.1)
+        print('||           \ N\\') #13.
+        t.sleep(0.1)
+        print('||            | G|') #14 | Middle/Medium.
+        t.sleep(0.1)
+        print('||           / S/') #15.
+        t.sleep(0.1)
+        print('||          / A/') #16.
+        t.sleep(0.1)
+        print('||         / T/') #17.
+        t.sleep(0.1)
+        print('||        / I/') #18.
+        t.sleep(0.1)
+        print('||       / S/') #19.
+        t.sleep(0.1)
+        print('||      / F/') #20.
+        t.sleep(0.1)
+        print('||     / Y/') #21.
+        t.sleep(0.1)
+        print('||    / I/') #22.
+        t.sleep(0.1)
+        print('||   / L/') #23.
+        t.sleep(0.1)
+        print('||  / O/') #24.
+        t.sleep(0.1)
+        print('|| / V/') #25.
+        t.sleep(0.1)
+        print('||/ E/') #26.
+        t.sleep(0.1)
+        print('|| U/') #27 | Meeting Point 2.
+        t.sleep(0.1)
+Satisfying1()
